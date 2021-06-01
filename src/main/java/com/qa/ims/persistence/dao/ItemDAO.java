cf7f541022ce9dd60520db2a51efa2aeb408693e
David <davidacosta25081@yahoo.com>
2021-05-31 20:59:27 -0500

    Add update() and delete() methods to ItemDAO

--- src/main/java/com/qa/ims/persistence/dao/ItemDAO.java
+++ src/main/java/com/qa/ims/persistence/dao/ItemDAO.java
@@ -1,5 +1,7 @@
 package com.qa.ims.persistence.dao;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -26,9 +28,9 @@ public class ItemDAO implements Dao<Item> {
 		}
 
 		/**
-		 * Reads all customers from the database
+		 * Reads all items from the database
 		 * 
-		 * @return A list of customers
+		 * @return A list of items
 		 */
 		@Override
 		public List<Item> readAll() {
@@ -60,6 +62,84 @@ public class ItemDAO implements Dao<Item> {
 			return null;
 		}
 
+       
+		@Override
+		public Item create(Item item) {
+			try (Connection connection = DBUtils.getInstance().getConnection();
+					PreparedStatement statement = connection
+							.prepareStatement("INSERT INTO items(item_name, item_description, item_price) VALUES (?, ?, ?)");) {
+				statement.setString(1, item.getItemName());
+				statement.setString(2, item.getItemDescription());
+				statement.setDouble(3, item.getItemPrice());
+				statement.executeUpdate();
+				return readLatest();
+			} catch (Exception e) {
+				LOGGER.debug(e);
+				LOGGER.error(e.getMessage());
+			}
+			return null;
+		}
+
+		@Override
+		public Item read(Long id) {
+			try (Connection connection = DBUtils.getInstance().getConnection();
+					PreparedStatement statement = connection.prepareStatement("SELECT * FROM items WHERE id = ?");) {
+				statement.setLong(1, id);
+				try (ResultSet resultSet = statement.executeQuery();) {
+					resultSet.next();
+					return modelFromResultSet(resultSet);
+				}
+			} catch (Exception e) {
+				LOGGER.debug(e);
+				LOGGER.error(e.getMessage());
+			}
+			return null;
+		}
+
+		/**
+		 * Updates an Item in the database
+		 * 
+		 * @param item - takes in an item object, the id field will be used to
+		 *                 update that item in the database
+		 * @return
+		 */
+		@Override
+		public Item update(Item item) {
+			try (Connection connection = DBUtils.getInstance().getConnection();
+					PreparedStatement statement = connection
+							.prepareStatement("UPDATE items SET item_name = ?, item_description = ?, item_price = ? WHERE id = ?");) {
+				statement.setString(1, item.getItemName());
+				statement.setString(2, item.getItemDescription());
+				statement.setDouble(3, item.getItemPrice());
+				statement.setLong(4, item.getId());
+				statement.executeUpdate();
+				return read(item.getId());
+			} catch (Exception e) {
+				LOGGER.debug(e);
+				LOGGER.error(e.getMessage());
+			}
+			return null;
+		}
+
+		/**
+		 * Deletes an item in the database
+		 * 
+		 * @param id - id of the item
+		 */
+		@Override
+		public int delete(long id) {
+			try (Connection connection = DBUtils.getInstance().getConnection();
+					PreparedStatement statement = connection.prepareStatement("DELETE FROM items WHERE id = ?");) {
+				statement.setLong(1, id);
+				return statement.executeUpdate();
+			} catch (Exception e) {
+				LOGGER.debug(e);
+				LOGGER.error(e.getMessage());
+			}
+			return 0;
+		}
+
+	}
 
 
 
@@ -79,7 +159,3 @@ public class ItemDAO implements Dao<Item> {
 
 
 
-
-
-
-}
