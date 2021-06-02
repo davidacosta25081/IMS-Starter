4b4b398e650f3c6f4e36515bea1935b392ad20af
David <davidacosta25081@yahoo.com>
2021-06-02 00:09:22 -0500

    Add orderDAO and OrderDAO classes

---
+++ src/main/java/com/qa/ims/persistence/dao/OrderItemDAO.java
@@ -0,0 +1,60 @@
+package com.qa.ims.persistence.dao;
+
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.qa.ims.persistence.domain.OrderItem;
+
+public class OrderItemDAO implements Dao<OrderItem> {
+
+	@Override
+	public List<OrderItem> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public OrderItem read(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public OrderItem create(OrderItem t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public OrderItem update(OrderItem t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delete(long id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public OrderItem modelFromResultSet(ResultSet resultSet) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
